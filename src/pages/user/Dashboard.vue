67963f505e785a18f6949d2853c420327864c9f2
Paulo Sergio <pauloserg861@gmail.com>
2022-10-06 22:14:06 -0300

    importando header da pagina

--- src/pages/user/Dashboard.vue
+++ src/pages/user/Dashboard.vue
@@ -1,13 +1,16 @@
 <template>
-  <div>
-    Dashboard de usuario
-  </div>
+  <Header />
+    <main>
+      ola
+    </main>
 </template>
 
 <script lang="ts">
+import Header from '@/components/components-user/Header.vue'
 import { defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'Dashboard',
+  components: { Header },
 })
 </script>
