f5f57aea217367285fe3d38e8a0a5968def6a9b5
Paulo Sergio <pauloserg861@gmail.com>
2022-10-06 21:55:03 -0300

    criando rota e view para dashboard

---
+++ src/pages/user/Dashboard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Dashboard de usuario
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Dashboard',
+})
+</script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Login from '../pages/Login.vue'
+import Dashboard from '../pages/user/Dashboard.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,16 @@ const router = createRouter({
       name: 'login',
       component: Login
     },
+    {
+      path: '/app',
+      children: [
+        {  
+          path: 'admin',
+          name: 'Dashboard',
+          component: Dashboard,
+        }
+      ]
+    }
   ]
 })
 
