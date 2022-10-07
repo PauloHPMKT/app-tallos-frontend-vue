b7a1c9038592649afb6fe08e06415188ca8254b4
Paulo Sergio <pauloserg861@gmail.com>
2022-10-06 22:14:21 -0300

    criando header de usuario

---
+++ src/components/components-user/Header.vue
@@ -0,0 +1,54 @@
+<template>
+  <header class="user-header">
+    <div class="useful-links">
+      <a href="#">Tallos blog</a>
+      <a href="#">FAQ</a>
+    </div>
+    <div>
+      <router-link to="/app/admin">
+        <img src="/img/tallos-logo-(1).png" alt="Logo Menu principal" class="logo-menu">
+      </router-link>
+    </div>
+    <div>
+      card de perfil de usuario
+    </div>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Header',
+})
+</script>
+
+<style scoped>
+.user-header {
+  width: 100%;
+  padding: 26px 80px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #fff;
+  filter: drop-shadow(2px -2px 5px #808080);
+}
+
+.useful-links a {
+  padding: 3px 10px;
+  border: 1px solid #009acc;
+  border-radius: 20px;
+  margin: 0 5px;
+  color: #009acc;
+  transition: .3s ease;
+}
+
+.useful-links a:hover {
+  background-color: #009acc;
+  color: #fff;
+} 
+
+.user-header .logo-menu {
+  width: 50px;
+}
+</style>
