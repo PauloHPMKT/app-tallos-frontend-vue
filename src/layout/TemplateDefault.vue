499de57c3b16f6654b8817510fb693564f3ad945
Paulo Sergio <pauloserg861@gmail.com>
2022-11-05 16:18:28 -0300

    criando estrutura de template de pagina

---
+++ src/layout/TemplateDefault.vue
@@ -0,0 +1,16 @@
+<template>
+  <Header />
+  <main>
+    <router-view />
+  </main>
+</template>
+
+<script lang="ts">
+import Header from '@/components/components-user/Header.vue'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  components: { Header },
+  name: 'TemplateDefault',
+})
+</script>
