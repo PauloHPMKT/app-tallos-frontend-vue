2281f0de7eccf9d5d33d525637b0014fd88dc326
Paulo Sergio <pauloserg861@gmail.com>
2022-11-05 20:16:54 -0300

    estilizando card-dashboard

--- src/components/components-user/cards/CardDashboard.vue
+++ src/components/components-user/cards/CardDashboard.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="card_bashboard">
-    <div>
-      teste
+    <div class="content_card">
+      <Icon icon="carbon:user-avatar" />
+      <p>Quantidade de usuários em nossa plataforma:</p>
+      <span>25</span>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { Icon } from '@iconify/vue'
 
 export default defineComponent({
     name: 'CardDashboard',
+    components: { Icon },
 })
 </script>
