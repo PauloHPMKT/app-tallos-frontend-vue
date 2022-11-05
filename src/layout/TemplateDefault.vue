e90608fa0debef2156cc733e902c1d34306288da
Paulo Sergio <pauloserg861@gmail.com>
2022-11-05 16:30:45 -0300

    implementando estilos ao template

--- src/layout/TemplateDefault.vue
+++ src/layout/TemplateDefault.vue
@@ -1,6 +1,6 @@
 <template>
   <Header />
-  <main>
+  <main class="container_template">
     <router-view />
   </main>
 </template>
@@ -14,3 +14,5 @@ export default defineComponent({
   name: 'TemplateDefault',
 })
 </script>
+
+<style lang="scss" scoped></style>
