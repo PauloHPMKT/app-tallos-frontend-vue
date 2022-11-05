4f1c66f6f6de40e470c8ce477290b5a9994ef43b
Paulo Sergio <pauloserg861@gmail.com>
2022-11-05 19:17:17 -0300

    criando card da dashboard e estrutura de conteudo

---
+++ src/components/components-user/ManagerOverview.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="meneger_card">
+    <CardDashboard />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import CardDashboard from './cards/CardDashboard.vue'
+
+export default defineComponent({
+  components: { CardDashboard },
+  name: 'ManagerOverview',
+})
+</script>
---
+++ src/components/components-user/cards/CardDashboard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="card_bashboard">
+    Aqui vai os cards do usuario viu
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'CardDashboard',
+})
+</script>
