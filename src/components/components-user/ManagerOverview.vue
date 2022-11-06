5d69dd0dc788eebeef43a53c874348e3a8d0847c
Paulo Sergio <pauloserg861@gmail.com>
2022-11-05 21:15:01 -0300

    importando estrutura de tipos

--- src/components/components-user/ManagerOverview.vue
+++ src/components/components-user/ManagerOverview.vue
@@ -1,15 +1,25 @@
 <template>
   <div class="meneger_card">
-    <CardDashboard />
+    <CardDashboard :data_user="message_user"/>
+    <CardDashboard :data_user="message_chat"/>
+    <CardDashboard :data_user="message_admin"/>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import CardDashboard from './cards/CardDashboard.vue'
+import { Messages } from '../../types/enums/enums'
 
 export default defineComponent({
   components: { CardDashboard },
   name: 'ManagerOverview',
+  data() {
+    return {
+      message_user: Messages.message_user,
+      message_chat: Messages.message_chat,
+      message_admin: Messages.message_admin,
+    }
+  }
 })
 </script>
