d9dcd07e5074bdbec74a76c4374f71a25eaa7672
Paulo Sergio <pauloserg861@gmail.com>
2022-11-05 21:13:49 -0300

    criando diretorio de tipos

---
+++ src/types/enums/enums.ts
@@ -0,0 +1,5 @@
+export enum Messages {
+  message_user = 'Quantidade de usuários em nossa plataforma:',
+  message_chat = 'Quantidade de usuarios online no chat:',
+  message_admin = 'Quantidade de usuários administradores:',
+}
