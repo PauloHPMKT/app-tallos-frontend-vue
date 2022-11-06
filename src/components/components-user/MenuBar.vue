10c4780b36c811a474633d2785bb0817b197e400
Paulo Sergio <pauloserg861@gmail.com>
2022-11-05 21:14:34 -0300

    criando estrutura do menu

--- src/components/components-user/MenuBar.vue
+++ src/components/components-user/MenuBar.vue
@@ -1,13 +1,36 @@
 <template>
-  <div>
-    Menu de navegacao da plataforma
+  <div class="meneger_card">
+    <nav>
+      <ul class="menu_list">
+        <li>
+          <router-link to="/">
+            <Icon icon="akar-icons:people-group" />
+            <span>Acessar usuários</span>  
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/">
+            <Icon icon="bi:chat-dots" />
+            <span>Acessar chat</span>
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/">
+            <Icon icon="icons8:support" />
+            <span>Suporte</span>
+          </router-link>
+        </li>
+      </ul>
+    </nav>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { Icon } from '@iconify/vue'
 
 export default defineComponent({
   name: 'MenuBar',  
+  components: { Icon }
 })
 </script>
