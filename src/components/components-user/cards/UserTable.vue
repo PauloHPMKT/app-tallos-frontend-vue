47d53a7527a10a23663e0625d75b1af929620158
Paulo Sergio <pauloserg861@gmail.com>
2022-11-12 15:43:43 -0300

    criando componente de tabela

---
+++ src/components/components-user/cards/UserTable.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="table-container">
+    <table>
+      <thead>
+        <tr>
+          <th>Matricula</th>
+          <th>Nome</th>
+          <th>E-mail corporativo</th>
+          <th>Acoes</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>650</td>
+          <td>Paulo Sérgio</td>
+          <td>paulo.sergio@tallos.com</td>
+          <td>Opcoes</td>
+        </tr>
+        <tr>
+          <td>650</td>
+          <td>Paulo Sérgio</td>
+          <td>paulo.sergio@tallos.com</td>
+          <td>Opcoes</td>
+        </tr>
+        <tr>
+          <td>650</td>
+          <td>Paulo Sérgio</td>
+          <td>paulo.sergio@tallos.com</td>
+          <td>Opcoes</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'UserTable',
+})
+</script>
