3524e91b90ea7bda65369e96c51a285417a3c362
Paulo Sergio <pauloserg861@gmail.com>
2022-11-12 12:09:36 -0300

    criando estrutura de formulario

--- src/components/components-user/Forms/FormRegister.vue
+++ src/components/components-user/Forms/FormRegister.vue
@@ -1,27 +1,51 @@
 <template>
   <div class="form-register-contact">
-    <div>
-      <Icon icon="bxs:user-plus" />
+    <div class="icon-container">
+      <Icon icon="mi:user-add" />
       <span>Novo Contato</span>
     </div>
     <form @submit.prevent="handleSubmitNewUser">
-      <div @click="hiddenPersonalInfo"> 
-        <span>Informacoes Pessoais</span>
-        <div v-if="hiddenPersonal">
-          <input type="text" name="" id="">
-          <input type="text" name="" id="">
+      <div class="tag_form_info"> 
+        <span @click="hiddenPersonalInfo">Informacoes Pessoais</span>
+        <div class="inputs-container" v-if="hiddenPersonal">
+          <div>
+            <label>Nome Completo</label>
+            <input type="text" placeholder="Ex: João Carlos">
+          </div>
+          <div>
+            <label>E-mail</label>
+            <input type="text" placeholder="Ex: meuemail@example.com">
+          </div>
+          <div>
+            <label>Contato</label>
+            <input type="text" placeholder="Ex: Somente números">
+          </div>
+          <div>
+            <label>CPF</label>
+            <input type="text" placeholder="Ex: 000.000.000-00">
+          </div>
+          <div>
+            <label>Matricula</label>
+            <input type="text" placeholder="Crie uma matricula Ex: Somente números">
+            <!--criar campo para retornar ultima matricula registrada-->
+          </div>
+          <div>
+            <label>Descricao</label>
+            <textarea placeholder="Informacoes sobre o contato"></textarea>
+            <!--criar campo para retornar ultima matricula registrada-->
+          </div>
         </div>
       </div>
-      <div @click="hiddenAddressInfo"> 
+      <div class="tag_form_info" @click="hiddenAddressInfo"> 
         <span>Endereco</span>
-        <div v-if="hiddenAddress">
+        <div class="inputs-container" v-if="hiddenAddress">
           <input type="text" name="" id="">
           <input type="text" name="" id="">
         </div>
       </div>
-      <div @click="hiddenCompanyInfo"> 
+      <div class="tag_form_info" @click="hiddenCompanyInfo"> 
         <span>Organizacão</span>
-        <div v-if="hiddenCompany">
+        <div class="inputs-container" v-if="hiddenCompany">
           <input type="text" name="" id="">
           <input type="text" name="" id="">
         </div>
