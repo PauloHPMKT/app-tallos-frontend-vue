cc4950950722e16197fdd72b09402deff0d171e4
Paulo Sergio <pauloserg861@gmail.com>
2022-11-11 23:15:38 -0300

    criando componente de tipografia

---
+++ src/components/components-user/Typography.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="typography-path">
+    <h1>Dashboard</h1>
+    <span>Indicadores da plataforma</span> 
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Typography',
+})
+</script>
