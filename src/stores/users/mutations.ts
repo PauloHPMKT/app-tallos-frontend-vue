96698abff07b6496590956ffe938d0d3a48c1d8c
Paulo Sergio <pauloserg861@gmail.com>
2022-11-13 16:28:45 -0300

    criando mutations

--- src/stores/users/mutations.ts
+++ src/stores/users/mutations.ts
@@ -1,6 +1,13 @@
 export default {
-  GET_USERS(state:any, payload:any) {
-    state.user =  payload
-    console.log(state)
+  GET_USERS(state: any, payload:any) {
+    console.log(state, payload)
+    state.users = payload
+  },
+
+  LOGIN(state: any, payload: any) {
+    state.userName = payload.name
+    state.userEmail = payload.email
+    state.userId =  payload.sub
+    state.userAvatar = payload.avatar 
   }
 }
