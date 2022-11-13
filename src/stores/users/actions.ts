b2c9dbe097982ae624eebd6898bc327902e6be86
Paulo Sergio <pauloserg861@gmail.com>
2022-11-13 16:28:59 -0300

    criando actions

--- src/stores/users/actions.ts
+++ src/stores/users/actions.ts
@@ -1,10 +1,29 @@
 import userServices from "@/services/user-services"
+import jwtDecode from "jwt-decode"
 
 export default {
   async getUsers(context: any) {
+    console.log(context)
     await userServices.listUsers().then(res => {
-      console.log(res.data)
       context.commit('GET_USERS', res.data)
+    })
+  },
+
+  async login(context: any, data: object) {
+    console.log(data)
+    await userServices.login(data).then(res => {
+      console.log(res.data);
+      const validToken = res.data.access_token
+      
+      if (validToken) {
+        localStorage.setItem('token', validToken)
+
+        const decode = jwtDecode(validToken)
+        
+        context.commit('LOGIN', decode)
+
+        //location.replace('/app/admin')
+      }
     })
   }
 }
